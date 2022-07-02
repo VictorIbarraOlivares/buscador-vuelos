f2c54a01a69058eee07c9ca4a803bdd8bc881016
unknown <victor.ibarra.olivares@gmail.com>
2022-07-02 12:44:17 -0400

    se inicia el formulario

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,178 @@
-import { useState } from 'react'
+import { Formik } from 'formik';
 import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
-    <div className="App">
-      asd
+    <div className='bg-white px-4 py-5 sm:px-6'>
+      <Formik
+        initialValues={{
+          origen: '',
+          destino: '',
+          ida: '',
+          regreso: '',
+          adultos: 0,
+          boys: 0
+        }}
+        validate={values => {
+          const errors = {};
+          if (!values.origen) {
+            errors.origen = 'Requerido';
+          }
+          if (!values.destino) {
+            errors.destino = 'Requerido';
+          }
+
+          return errors;
+        }}
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <form onSubmit={handleSubmit} className="space-y-8 divide-y divide-gray-200">
+            <div className="space-y-8 divide-y divide-gray-200">
+              <div className="pt-8">
+                <div>
+                  <h3 className="text-lg leading-6 font-medium text-gray-900">Busqueda de vuelos</h3>
+                  <p className="mt-1 text-sm text-gray-500">Complete los campos necesarios para realizar la busqueda.</p>
+                </div>
+                <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
+                  <div className="sm:col-span-3">
+                    <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                      Origen (*)
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        name="origen"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.origen}
+                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                      />
+                      {errors.origen && touched.origen && errors.origen}
+                    </div>
+                  </div>
+
+                  <div className="sm:col-span-3">
+                    <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                      Destino (*)
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        name="last-name"
+                        id="last-name"
+                        autoComplete="family-name"
+                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="sm:col-span-3">
+                    <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                      Ida (*)
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        name="first-name"
+                        id="first-name"
+                        autoComplete="given-name"
+                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="sm:col-span-3">
+                    <label htmlFor="last-name" className="block text-sm font-medium text-gray-700">
+                      Regreso
+                    </label>
+                    <div className="mt-1">
+                      <input
+                        type="text"
+                        name="last-name"
+                        id="last-name"
+                        autoComplete="family-name"
+                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="sm:col-span-3">
+                    <label htmlFor="country" className="block text-sm font-medium text-gray-700">
+                      Adultos (*)
+                    </label>
+                    <div className="mt-1">
+                      <select
+                        id="country"
+                        name="country"
+                        autoComplete="country-name"
+                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                      >
+                        <option>United States</option>
+                        <option>Canada</option>
+                        <option>Mexico</option>
+                      </select>
+                    </div>
+                  </div>
+
+                  <div className="sm:col-span-3">
+                    <label htmlFor="country" className="block text-sm font-medium text-gray-700">
+                      Niños
+                    </label>
+                    <div className="mt-1">
+                      <select
+                        id="country"
+                        name="country"
+                        autoComplete="country-name"
+                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                      >
+                        <option>United States</option>
+                        <option>Canada</option>
+                        <option>Mexico</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+
+            <div className="pt-5">
+              <div className="flex justify-end">
+                <button
+                  type="button"
+                  className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Cancel
+                </button>
+                
+                <button
+                  type="submit"
+                  disabled={isSubmitting}
+                  className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  Save
+                </button>
+              </div>
+            </div>
+          </form>
+        )}
+      </Formik>
     </div>
   )
 }
