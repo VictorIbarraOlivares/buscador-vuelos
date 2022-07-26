d558d475a8cc4eff07bc7dcdcc08851be7560ebe
unknown <victor.ibarra.olivares@gmail.com>
2022-07-25 22:58:23 -0400

    arreglos

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -182,7 +182,7 @@ export default function Sidebar({ children }) {
 
           </div>
         </div>
-        <div className="md:pl-64 flex flex-col flex-1">
+        <div className="md:pl-64 h-screen flex flex-col flex-1">
           <div className="sticky top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-white">
             <button
               type="button"
